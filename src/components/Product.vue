8a5a9700b3faffe1d8fbb861effb578926e622d3
roiilan <roiilan23@gmail.com>
2022-05-03 12:17:30 +0300

    Initial commit

---
+++ src/components/Product.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product-container">
+        <div v-if="loaded">	   
+            <div class="card-body">
+                <p >ID: {{productId}}</p>
+                <p >Product Name: {{productName}}</p>
+                <p >Product Price: ${{productPrice}}</p>
+                <!-- <router-link :to="'/'" class="btn btn-primary"> Back </router-link> -->
+            </div>
+        </div>
+
+        <div v-else> 
+            <h3>Loading...</h3>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  data () {
+    return {
+    	productId:'26256226',
+    	productName: 'car',
+    	productPrice: '15',
+        productDetails:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, tenetur temporibus explicabo fugit, ipsa neque non eius porro quasi vero nulla aperiam voluptate. Voluptates, maxime! Doloribus et incidunt autem amet.',
+    	loaded: true,
+    }    
+  },
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Product />
+  </div>
+</template>
+
+<script>
+// import ProductDetails from '@/components/ProductDetails.vue'
+import Product from '@/components/Product.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Product,
+  }
+}
+</script>
